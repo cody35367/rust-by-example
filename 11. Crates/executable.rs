b47575c7946fad668f75d6c9470cf13dd2aa09d8
Cody Hodges <cody35367@gmail.com>
2025-06-15 20:47:23 -0500

    Finished 11, started 12

---
+++ 11. Crates/executable.rs
@@ -0,0 +1,14 @@
+// extern crate rary; // May be required for Rust 2015 edition or earlier
+
+fn main() {
+    rary::public_function();
+
+    // Error! `private_function` is private
+    //rary::private_function();
+
+    rary::indirect_access();
+}
+
+// Where library.rlib is the path to the compiled library, assumed that it's
+// in the same directory here:
+// $ rustc executable.rs --extern rary=library.rlib && ./executable 
